d347d8ce199cbc83af4127427fba29407e26d988
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2023-10-31 10:06:13 +0300

    CompleteViewComponents

---
+++ HotelProject/Frontend/Hotel.Frontend/ViewComponents/Default/ServicePartial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Frontend.ViewComponents.Default
+{
+	public class ServicePartial : ViewComponent
+	{
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
+
