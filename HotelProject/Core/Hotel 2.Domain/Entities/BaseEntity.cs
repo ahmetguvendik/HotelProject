2695d671099e2adc5663b70ff65408047dddde07
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2023-11-04 23:49:33 +0300

    StartingAboutPage

---
+++ HotelProject/Core/Hotel 2.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Hotel.Domain.Entities
+{
+	public class BaseEntity
+	{
+		public string Id { get; set; }	
+	}
+}
+
---
+++ HotelProject/Frontend/Hotel.Frontend/Controllers/AboutController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel.Frontend.Models.Newsletter;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Hotel.Frontend.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AboutController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult NewsletterPartial()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> NewsletterPartial(CreateNewsletterViewModel createNewsletterViewModel)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createNewsletterViewModel);
+            var stringContent = new StringContent(jsonData, encoding: Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7105/Subscribe", stringContent);
+            return View();
+        }
+    }
+}
+
